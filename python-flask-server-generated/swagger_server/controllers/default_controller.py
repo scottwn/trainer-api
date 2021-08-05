4032ca05b63ae688146fb3e4958da1108c400e5a
Scott Wyman Neagle <wymnea@protonmail.com>
2021-08-04 22:45:45 -0500

    basically working with cleaned up doc

--- python-flask-server-generated/swagger_server/controllers/default_controller.py
+++ python-flask-server-generated/swagger_server/controllers/default_controller.py
@@ -1,12 +1,25 @@
 import connexion
 import six
+import pymongo
+import os
 
 from swagger_server.models.trainer import Trainer  # noqa: E501
 from swagger_server import util
 
+from bson.objectid import ObjectId
+
+conn_str = os.environ["MONGOSERVER"]
+client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+db = client.cgdb
+trainers = db.trainers
+
+try:
+    print(client.server_info())
+except Exception:
+    print("Unable to connect to the server.")
+
 
 def create_trainer(body):  # noqa: E501
-    print('create trainer')
     """Create a new trainer.
 
      # noqa: E501
@@ -17,12 +30,14 @@ def create_trainer(body):  # noqa: E501
     :rtype: Trainer
     """
     if connexion.request.is_json:
-        body = Trainer.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        trainer_id = trainers.insert_one(body).inserted_id
+        body["id"] = str(trainer_id)
+        del body["_id"]
+        return body
+    return "Badly formed request."
 
 
-def get_trainer(id):  # noqa: E501
-    print('get trainer')
+def get_trainer(id_):  # noqa: E501
     """Get a trainer by ID.
 
      # noqa: E501
@@ -32,4 +47,7 @@ def get_trainer(id):  # noqa: E501
 
     :rtype: Trainer
     """
-    return 'do some magic!'
+    trainer = trainers.find_one({"_id": ObjectId(id_)})
+    trainer["id"] = id_
+    del trainer["_id"]
+    return trainer
